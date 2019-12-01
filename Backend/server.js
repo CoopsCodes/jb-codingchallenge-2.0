3f4d717610ead3483d3cb9f04c16e9026dd659fb
Cooper Viktor <im.cooperviktor@gmail.com>
2019-12-01 17:33:55 +1100

    api call debugging, fix params not being sent from the front end, then handle keys

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,52 @@
 const express = require("express");
+const axios = require("axios").default;
 const app = express();
 const port = 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// require("./routes")(app);
+const key1 = process.env.API_KEY1;
+const url = `http://api.openweathermap.org/data/2.5/weather?q=Melbourne,Australia&appid=7627c7ef2c6d05dc55f2076ceae3b282`;
+
+// app.post("/", (req, res) => {
+//   const url = `http://api.openweathermap.org/data/2.5/weather?q=${req.body.city},${req.body.country}${key1}`;
+//   console.log(req.body);
+// });
 
 app.get("/", (req, res) => {
-  res.send("PORT 5000");
+  axios
+    .get(url)
+    .then(res => {
+      console.log("Axios response", res);
+    })
+    .catch(error => {
+      console.log("error", error);
+    });
+  // const url = `http://api.openweathermap.org/data/2.5/weather?q=${req.body.city},${req.body.country}${key1}`;
+  console.log("req", req.body);
+  console.log("req.query", req.query);
 });
+
+// app.get("http://localhost:3000", (req, res) => {
+//   const url = "http://api.openweathermap.org/data/2.5/weather?q=";
+//   const key1 = process.env.API_KEY1;
+//   const key2 = process.env.API_KEY2;
+//   const key3 = process.env.API_KEY3;
+//   const key4 = process.env.API_KEY4;
+//   const key5 = process.env.API_KEY5;
+
+//   const locationData = (url1, city, country, key) => {
+//     let newUrl = url1 + city + "," + country + key1;
+//     return newUrl;
+//   };
+
+//   const apiUrl = locationData(url, city, country, key);
+
+//   axios.get(apiUrl).then(res => {
+//     console.log("Axios response", res.data);
+//   });
+// });
 
 app.listen(port, err => {
   if (err) {
--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -1,8 +1,86 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: "",
+      country: "",
+      key: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    // get our form data out of state
+    const { city, country, key } = this.state;
+    console.log("city", this.state.city);
+    axios
+      .get("http://localhost:5000", { city, country, key })
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+
+  render = () => {
+    return (
+      <div className="App">
+        <h1>Weather Search</h1>
+        <form className="form" onSubmit={this.handleSubmit}>
+          <div className="formFields">
+            <label>City</label>
+            <input
+              type="text"
+              name="city"
+              onChange={this.handleChange}
+              required
+            />
+          </div>
+          <div className="formFields">
+            <label>Country</label>
+            <input
+              type="text"
+              name="country"
+              onChange={this.handleChange}
+              required
+            />
+          </div>
+          <label>
+            Key Selections
+            <select name="key" onChange={this.handleChange}>
+              <option value="&appid=7627c7ef2c6d05dc55f2076ceae3b282">
+                Key 1
+              </option>
+              <option value="&appid=d8db4ef69d6910cebf4b52056eebd213">
+                Key 2
+              </option>
+              <option value="&appid=e607647f8751c7b2d7f7554ce610727a">
+                Key 3
+              </option>
+              <option value="&appid=25d5e04ba2b88e9f90e93113b47021d3">
+                Key 4
+              </option>
+              <option value="&appid=d4511417629f42378f67bd2e3430211f">
+                Key 5
+              </option>
+            </select>
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  };
 }
 
 export default App;
