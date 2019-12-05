561d112a2a7f2b633e33c6a1dbff4696c7b8819e
Cooper Viktor <im.cooperviktor@gmail.com>
2019-12-05 16:07:22 +1100

    push it, push it real good

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
 import "./App.css";
-const moment = require("moment");
-moment().format();
+// const moment = require("moment");
+// moment().format();
 
 class App extends Component {
   constructor(props) {
@@ -11,23 +11,23 @@ class App extends Component {
       city: "",
       country: "",
       key: "",
-      weather: [],
-      callsMade1: 0,
-      callsMade2: 0,
-      callsMade3: 0,
-      callsMade4: 0,
-      callsMade5: 0,
-      keyCalls: [],
-      lockOutTime1: '',
-      lockOutTime2: '',
-      lockOutTime3: '',
-      lockOutTime4: '',
-      lockOutTime5: '',
-      timeNow1: '',
-      timeNow2: '',
-      timeNow3: '',
-      timeNow4: '',
-      timeNow5: ''
+      weather: []
+      // callsMade1: 0,
+      // callsMade2: 0,
+      // callsMade3: 0,
+      // callsMade4: 0,
+      // callsMade5: 0,
+      // keyCalls: [],
+      // lockOutTime1: '',
+      // lockOutTime2: '',
+      // lockOutTime3: '',
+      // lockOutTime4: '',
+      // lockOutTime5: '',
+      // timeNow1: '',
+      // timeNow2: '',
+      // timeNow3: '',
+      // timeNow4: '',
+      // timeNow5: ''
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -46,59 +46,69 @@ class App extends Component {
           country: this.state.country,
           key: this.state.key
         }
-      })
+      });
       if (response.data.data.cod !== 200) {
-        return alert(response.data.data.message)
+        return alert(response.data.data.message);
       } else {
-        this.setState({ weather: response.data })
+        this.setState({ weather: response.data });
       }
-      console.log('GET response', response)
+      console.log("GET response", response);
     } catch (err) {
-      console.log('catch error', err)
-      return alert(err)
+      console.log("catch error", err);
+      return alert(err);
     }
   }
 
   handleSubmit(e) {
     e.preventDefault();
 
-    if (this.state.lockOutTime1 === "") {
-      this.setState({
-        lockOutTime1: moment().add(1, "hour").format("LLL")
-      });
-    } else if (this.state.lockOutTime2 === "") {
-      this.setState({
-        lockOutTime2: moment().add(1, "hour").format("LLL")
-      });
-    } else if (this.state.lockOutTime3 === "") {
-      this.setState({
-        lockOutTime3: moment().add(1, "hour").format("LLL")
-      });
-    } else if (this.state.lockOutTime4 === "") {
-      this.setState({
-        lockOutTime4: moment().add(1, "hour").format("LLL")
-      });
-    } else if (this.state.lockOutTime5 === "") {
-      this.setState({
-        lockOutTime5: moment().add(1, "hour").format("LLL")
-      });
-    }
+    // if (this.state.lockOutTime1 === "") {
+    //   this.setState({
+    //     lockOutTime1: moment()
+    //       .add(1, "hour")
+    //       .format("LLL")
+    //   });
+    // } else if (this.state.lockOutTime2 === "") {
+    //   this.setState({
+    //     lockOutTime2: moment()
+    //       .add(1, "hour")
+    //       .format("LLL")
+    //   });
+    // } else if (this.state.lockOutTime3 === "") {
+    //   this.setState({
+    //     lockOutTime3: moment()
+    //       .add(1, "hour")
+    //       .format("LLL")
+    //   });
+    // } else if (this.state.lockOutTime4 === "") {
+    //   this.setState({
+    //     lockOutTime4: moment()
+    //       .add(1, "hour")
+    //       .format("LLL")
+    //   });
+    // } else if (this.state.lockOutTime5 === "") {
+    //   this.setState({
+    //     lockOutTime5: moment()
+    //       .add(1, "hour")
+    //       .format("LLL")
+    //   });
+    // }
 
-    if (this.state.key === "KEY1" && this.state.timeNow1 === '') {
-      this.setState({ timeNow1: moment().format("LLL") });
-    }
-    if (this.state.key === "KEY2" && this.state.timeNow2 === '') {
-      this.setState({ timeNow2: moment().format("LLL") });
-    }
-    if (this.state.key === "KEY3" && this.state.timeNow3 === '') {
-      this.setState({ timeNow3: moment().format("LLL") });
-    }
-    if (this.state.key === "KEY4" && this.state.timeNow4 === '') {
-      this.setState({ timeNow4: moment().format("LLL") });
-    }
-    if (this.state.key === "KEY5" && this.state.timeNow5 === '') {
-      this.setState({ timeNow5: moment().format("LLL") });
-    }
+    // if (this.state.key === "KEY1" && this.state.timeNow1 === "") {
+    //   this.setState({ timeNow1: moment().format("LLL") });
+    // }
+    // if (this.state.key === "KEY2" && this.state.timeNow2 === "") {
+    //   this.setState({ timeNow2: moment().format("LLL") });
+    // }
+    // if (this.state.key === "KEY3" && this.state.timeNow3 === "") {
+    //   this.setState({ timeNow3: moment().format("LLL") });
+    // }
+    // if (this.state.key === "KEY4" && this.state.timeNow4 === "") {
+    //   this.setState({ timeNow4: moment().format("LLL") });
+    // }
+    // if (this.state.key === "KEY5" && this.state.timeNow5 === "") {
+    //   this.setState({ timeNow5: moment().format("LLL") });
+    // }
     // if (
     //   (this.state.key === "KEY1" && this.state.callsMade1 <= 4 &&
     //     this.state.lockOutTime1 === "" &&
@@ -113,26 +123,36 @@ class App extends Component {
     //   alert(`You've used all the key allocations on this selection right now`);
     // }
 
-    if (
-      (this.state.key === "KEY1" && this.state.callsMade1 <= 4) || (this.state.key === "KEY1" && this.state.callsMade1 <= 4 && this.state.lockOutTime1 > this.state.timeNow1)
-    ) {
-      this.callApi()
-      this.setState(state => ({
-        callsMade1: state.callsMade1 + 1
-      }));
-    } else {
-      alert(`You've used all the key allocations on this selection right now please try again after ${this.state.lockOutTime1}`);
-    }
+    // if (
+    //   (this.state.key === "KEY1" && this.state.callsMade1 <= 4) ||
+    //   (this.state.key === "KEY1" &&
+    //     this.state.callsMade1 <= 4 &&
+    //     this.state.lockOutTime1 > this.state.timeNow1)
+    // ) {
+    //   this.callApi();
+    //   this.setState(state => ({
+    //     callsMade1: state.callsMade1 + 1
+    //   }));
+    // } else {
+    //   alert(
+    //     `You've used all the key allocations on this selection right now please try again after ${this.state.lockOutTime1}`
+    //   );
+    // }
 
-    if (
-      (this.state.key === "KEY2" && this.state.callsMade2 <= 4) || (this.state.key === "KEY2" && this.state.callsMade2 <= 4 && this.state.lockOutTime2 > this.state.timeNow2)
-    ) {
-      this.setState(state => ({
-        callsMade2: state.callsMade2 + 1
-      }));
-    } else {
-      alert(`You've used all the key allocations on this selection right now please try again after ${this.state.lockOutTime2}`);
-    }
+    // if (
+    //   (this.state.key === "KEY2" && this.state.callsMade2 <= 4) ||
+    //   (this.state.key === "KEY2" &&
+    //     this.state.callsMade2 <= 4 &&
+    //     this.state.lockOutTime2 > this.state.timeNow2)
+    // ) {
+    //   this.setState(state => ({
+    //     callsMade2: state.callsMade2 + 1
+    //   }));
+    // } else {
+    //   alert(
+    //     `You've used all the key allocations on this selection right now please try again after ${this.state.lockOutTime2}`
+    //   );
+    // }
 
     //   if (
     //     (this.state.callsMade3 <= 4 &&
@@ -175,6 +195,7 @@ class App extends Component {
     //   } else {
     //     alert(`You've used all the key allocations on this selection right now`);
     //   }
+    this.callApi();
   }
 
   render = () => {
@@ -204,10 +225,8 @@ class App extends Component {
             </div>
             <label>
               Key Selections
-            <select name="key" onChange={this.handleChange}>
-                <option>
-                  Select a key from the list below
-              </option>
+              <select name="key" onChange={this.handleChange}>
+                <option>Select a key from the list below</option>
                 <option value="KEY1">Key 1</option>
                 <option value="KEY2">Key 2</option>
                 <option value="KEY3">Key 3</option>
@@ -220,17 +239,20 @@ class App extends Component {
           <div>
             {/* Display here */}
             {/* <h1>Weather Response</h1> */}
-            {
-              weather.length === 0
-                ? <p>Search:</p>
-                : <ul>
-                  <p>Results:</p>{weather.data.weather.map(w => <li key={w.id}>{w.description}</li>)}
-                </ul>
-            }
+            {weather.length === 0 ? (
+              <p>Search:</p>
+            ) : (
+              <ul>
+                <p>Results:</p>
+                {weather.data.weather.map(w => (
+                  <li key={w.id}>{w.description}</li>
+                ))}
+              </ul>
+            )}
           </div>
         </div>
       </div>
-    )
-  }
+    );
+  };
 }
 export default App;
