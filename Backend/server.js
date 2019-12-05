8e2d051d4a561e83110f992d4ae3835cdc0abb77
Cooper Viktor <im.cooperviktor@gmail.com>
2019-12-05 17:01:14 +1100

    individual api limited to 5 calls

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const fetch = require("node-fetch");
-var cors = require('cors');
+const cors = require("cors");
 const app = express();
 const port = 5000;
 
 // .env use to abstract the keys
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 const result = dotenv.config();
 if (result.error) {
   throw result.error;
@@ -16,37 +16,53 @@ module.exports = envs;
 // express initialisation
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Oh hai Mark, I didnt see you there')
-})
+app.get("/", (req, res) => {
+  res.send("Oh hai Mark, I didnt see you there");
+});
 
-
+const apiKeyStore = {};
+exceededApiUsage = key => {
+  if (apiKeyStore[key] === undefined) {
+    apiKeyStore[key] = 0;
+  }
+  if (apiKeyStore[key] >= 5) {
+    return true;
+  } else {
+    apiKeyStore[key] += 1;
+    return false;
+  }
+};
 // the get request processing the URL request
 app.get("/weather", cors(), async (req, res, next) => {
   let city = req.query.city;
   let country = req.query.country;
-  let keyParam = req.query.key
+  let keyParam = req.query.key;
   switch (keyParam) {
     case "KEY1":
-      key = process.env.API_KEY1
+      key = process.env.API_KEY1;
       break;
     case "KEY2":
-      key = process.env.API_KEY2
+      key = process.env.API_KEY2;
       break;
     case "KEY3":
-      key = process.env.API_KEY3
+      key = process.env.API_KEY3;
       break;
     case "KEY4":
-      key = process.env.API_KEY4
+      key = process.env.API_KEY4;
       break;
     case "KEY5":
-      key = process.env.API_KEY5
+      key = process.env.API_KEY5;
       break;
     default:
-      key = "NA"
+      key = "NA";
   }
+  if (exceededApiUsage(key)) {
+    res.status(429).send({ err: "Exceeded API Usaged Amount" });
+    return;
+  }
+
   const url = `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}${key}`;
   const fetchData = fetch(url)
     .then(res => res.json())
@@ -54,11 +70,11 @@ app.get("/weather", cors(), async (req, res, next) => {
       res.send({ data });
     })
     .catch(err => {
-      console.log('Error', err)
-      res.send({ err })
+      console.log("Error", err);
+      res.send({ err });
     });
-  return fetchData
-})
+  return fetchData;
+});
 
 app.listen(port, err => {
   if (err) {
